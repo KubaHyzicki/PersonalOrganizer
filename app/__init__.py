e135dab84a0ea4ecad79a6bf209649477bfdab43
KubaHyzicki <kuba.hyzicki@gmail.com>
2019-12-16 14:58:25 +0100

    working init version

--- app/__init__.py
+++ app/__init__.py
@@ -17,15 +17,31 @@ class Database:
         self.con = pymysql.connect(host=host, user=user, password=password, db=db, cursorclass=pymysql.cursors.
                                    DictCursor)
         self.cur = self.con.cursor()
+
     def get_users(self):
         self.cur.execute("SELECT * FROM User")
         result = self.cur.fetchall()
+        print("ciastko {}".format(result))
         return result
 
-db = Database()
+    def add_user(self):
+        query="INSERT INTO User (lastname,email,password,firstName,permissions) VALUES ('Ciastek','ciastek@gmail.com','admin','Pan','client')"
+        self.cur.execute(query)
+        self.con.commit()
+
+#db = Database()
 
 @app.route('/')
 def default():
+    return "hello"
+@app.route('/viewAllUsers')
+def viewAllUsers():
     db = Database()
+    print('ciastko added')
     users = db.get_users()
-    return users
+    return str(users)
+@app.route('/addDefaultUser')
+def addUser():
+    db = Database()
+    db.add_user()
+    return 'Commited!'

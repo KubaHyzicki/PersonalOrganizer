b9309b0ef5a2970fbd8a31629ed87db7a2fba574
KubaHyzicki <kuba.hyzicki@gmail.com>
2020-01-09 15:03:42 +0100

    update requirements; remove obsolate config.py's

--- admin_config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    HOST = os.getenv('HOST')
-    DB_USER = os.getenv('ADMIN_DB_USER')
-    DB_PASS = os.getenv('ADMIN_DB_PASS')
-    DB_NAME = os.getenv('DB_NAME')
-    SQLALCHEMY_DATABASE_URI = 'mysql://' + str(DB_USER) + ':' + str(DB_PASS) + '@localhost/' + str(DB_NAME)
-    DEBUG = True
